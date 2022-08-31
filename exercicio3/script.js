fb9c22a2b59f652f270a4f241e2c7b621dee7824
Juliana <juliana-marques-@hotmail.com>
2022-08-31 14:22:47 -0300

    Exercício 8 feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -3,7 +3,8 @@ const operaçao2 = 5 === 5 || 5 === "5" //PORQUE ISSO É VERDADEIRO?
 const operaçao3 = !20 > 50
 const operaçao4 = !(20 > 50)
 
-console.log(operaçao1,
-    operaçao2,
-    operaçao3,
-    operaçao4)
+ console.log(operaçao1,
+     operaçao2,
+     operaçao3,
+     operaçao4)
+
